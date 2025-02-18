f60df1bd8586d3e3d74f66cf6046d3083e6424a5
Johnbarrymoreunong <johnbarrymoreunong@gmail.com>
2025-02-18 12:56:16 +0800

    Bonnel

---
+++ database/migrations/2025_02_15_074823_create_bookings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("user_id");
+            $table->foreign("user_id")->references("id")->on("users")->onDelete("cascade");
+            $table->unsignedBigInteger("room_id");
+            $table->foreign("room_id")->references("id")->on("rooms")->onDelete("cascade");
+            $table->unsignedBigInteger("payment_id");
+            $table->foreign("payment_id")->references("id")->on("payments")->onDelete("cascade");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
